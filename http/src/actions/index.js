c7922a6b1e871c0aed066947a45898c678f87a22
JASPINDER SING <jaspindersingh83@gmail.com>
2017-12-16 16:50:54 -0800

    Final Commit

--- http/src/actions/index.js
+++ http/src/actions/index.js
@@ -4,4 +4,11 @@ export const GET_FRIENDS = 'GET_FRIENDS';
 
 export const getFriends = () => {
     // Add the code for this action
+    let payload = {};
+    const apiUrl = 'http://localhost:5000/friends';
+    const friendRequest = axios.get(apiUrl)
+    return {
+        type: GET_FRIENDS,
+        payload: friendRequest
+    }
 };
